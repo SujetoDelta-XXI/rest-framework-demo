5c9a66b2e6f4de6deed9efc855ac9e964996ee35
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-06 21:43:59 -0500

    project

--- API_CRUD/settings.py
+++ API_CRUD/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
     'rest_framework',
     'projects.apps.ProjectsConfig',
 ]
