e010fdb779a2c0fdefa6a9cfa0ef795fe7304fe0
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-06 22:00:05 -0500

    A

--- API_CRUD/settings.py
+++ API_CRUD/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    technologies = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    technology = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
